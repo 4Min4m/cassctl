c4044a54ba92bb42cb67dac7644286dc26a267a2
Amin <36745993+4Min4m@users.noreply.github.com>
2026-09-10 17:53:45 +0000

    added callback

--- cassctl/cli.py
+++ cassctl/cli.py
@@ -1,8 +1,20 @@
 import typer
-from cassctl.commands import health as health_cmd
+from cassctl.commands.health import health
 
-app = typer.Typer(help="cassctl - Cassandra cluster opertaion check")
-app.command("health")(health_cmd.health)
+app = typer.Typer(
+    help="cassctl - Cassandra cluster operation check",
+    no_args_is_help=True,
+)
+
+@app.callback()
+def main() -> None:
+    """cassctl - Cassandra cluster operation check"""
+    pass
+
+@app.command("health")
+def health_cmd() -> None:
+    """Show Cassandra cluster health."""
+    health()
 
 if __name__ == "__main__":
     app()
